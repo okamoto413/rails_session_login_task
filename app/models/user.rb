8a97008d8ef3a75f114548c019006ed4f4716b75
kujira12 <ha9botan@gmail.com>
2024-04-11 16:57:13 +0900

    ツイートアプリケーション課題　再提出2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
 #8.機能要件：名前が未入力の場合	名前を入力してください
-  validates :name, presence: { message:"名前を入力してください"}
+  validates :name, presence: true
 
 #8.機能要件：メールアドレスが未入力の場合	メールアドレスを入力してください
-  validates :email, presence: { message:"メールアドレスを入力してください"}, length: { maximum: 255 }, uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
 
   #8.機能要件：メールアドレスがすでに使用されていた場合	メールアドレスはすでに使用されています
   validates :email, presence: {message:"メールアドレスはすでに使用されています"}, uniqueness: true
@@ -15,14 +15,12 @@ class User < ApplicationRecord
   has_secure_password
 
 #8.機能要件：パスワードが未入力の場合	パスワードを入力してください
-  validates :password,presence: { message:"パスワードを入力してください"}
-
-
+  validates :password,presence: true
 #8.機能要件：パスワードが6文字未満の場合	パスワードは6文字以上で入力してください
-  validates :password, length: { minimum: 6,  message:"パスワードは6文字以上で入力してください"}
+  validates :password, length: { minimum: 6}
 
 
 #8.機能要件：パスワードとパスワード（確認）が一致しない場合	パスワード（確認）とパスワードの入力が一致しません
-   validates :password, confirmation:{ message:"パスワード（確認）とパスワードの入力が一致しません"}, uniqueness: true
+  #  validates :password_confirmation, uniqueness: true
 
 end
